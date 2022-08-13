28fda1f2fb569c60ba30670b7ac524d80a7e4d2f
EMachad0 <elitonmachadod200@gmail.com>
2022-08-13 15:17:07 -0300

    🎉

---
+++ src/bin/client.rs
@@ -0,0 +1,73 @@
+use mini_redis::client;
+use tokio::sync::{mpsc, oneshot};
+
+use tokio_redis::command::Command;
+
+#[tokio::main]
+async fn main() {
+    // Create a mult-provider single-source channel with a capacity of at most 32.
+    let (tx, mut rx) = mpsc::channel(32);
+
+    // The `Sender` handles are moved into the tasks.
+    // As there are two tasks, we need a second `Sender`.
+    let tx2 = tx.clone();
+
+    // Spawn two tasks, one gets a key, the other sets a key
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "hello".to_string(),
+            resp: resp_tx,
+        };
+
+        // Send the GET request
+        tx.send(cmd).await.unwrap();
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+
+        // Send the SET request
+        tx2.send(cmd).await.unwrap();
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    let manager = tokio::spawn(async move {
+        // Establish a connection to the server
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+
+            match cmd {
+                Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+                Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
---
+++ src/command.rs
@@ -0,0 +1,20 @@
+use bytes::Bytes;
+use tokio::sync::oneshot;
+
+/// Provided by the requester and used by the manager task to send
+/// the command response back to the requester.
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+
+/// Multiple different commands are multiplexed over a single channel.
+#[derive(Debug)]
+pub enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
